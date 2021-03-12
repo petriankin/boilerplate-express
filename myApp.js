85064676af1d94e7cdf0df5064a2774ad35d3ba0
sergei_petriankin <Sergei_petriankin@epam.com>
2021-03-12 22:03:35 +0300

    stylesheet served. minor refactoring

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello World!");
-app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
+app.get("/", function (req, res) {
+ return res.sendFile(__dirname + "/views/index.html");
+})
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
