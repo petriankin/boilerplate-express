a10ce1d0a85f34d490a4167ea95070dbe5a381c6
sergei_petriankin <Sergei_petriankin@epam.com>
2021-03-12 21:02:49 +0300

    GET root handler updated to serve html page

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 console.log("Hello World!");
-app.get("/", (req, res) => res.send("Hello Express"))
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
 
 
 
