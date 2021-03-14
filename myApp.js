db15790afffe6fbdfb0dc471f8f9b347eae6c474
sergei_petriankin <Sergei_petriankin@epam.com>
2021-03-14 20:06:21 +0300

    /json handler added

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ console.log("Hello World!");
 app.get("/", function (req, res) {
  return res.sendFile(__dirname + "/views/index.html");
 })
+app.get("/json", function (req, res) {
+ return res.json('{"message": "Hello json"}');
+})
 app.use("/public", express.static(__dirname + "/public"));
 
 
